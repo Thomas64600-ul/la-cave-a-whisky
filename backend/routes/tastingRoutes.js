04317aa5430f585b9a70f34fb8248de0ef725423
Thomas64600-ul <thomasdetraversaygmail.comm>
2025-11-13 15:28:48 +0100

    Test Postman-validation du crud tasting

--- backend/routes/tastingRoutes.js
+++ backend/routes/tastingRoutes.js
@@ -9,18 +9,23 @@ import {
 } from "../controllers/tastingController.js";
 
 import { validate } from "../middlewares/validationMiddleware.js";
-import { tastingSchema } from "../schemas/tastingSchema.js";
+import { tastingSchema, tastingUpdateSchema } from "../schemas/tastingSchema.js";
 import { protect } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/", getAllTastings);
+
+router.get("/mine", protect, getUserTastings);
+
 router.get("/:id", getTastingById);
 
-router.get("/mine", protect, getUserTastings); 
 router.post("/", protect, validate(tastingSchema), createTasting);
-router.put("/:id", protect, validate(tastingSchema), updateTasting);
+
+router.put("/:id", protect, validate(tastingUpdateSchema), updateTasting);
+
 router.delete("/:id", protect, deleteTasting);
 
 export default router;
+
 
--- backend/schemas/tastingSchema.js
+++ backend/schemas/tastingSchema.js
@@ -1,11 +1,9 @@
 import Joi from "joi";
 
 export const tastingSchema = Joi.object({
-  whisky: Joi.string()
-    .required()
-    .messages({
-      "string.empty": "Le whisky est obligatoire.",
-    }),
+  whisky: Joi.string().required().messages({
+    "string.empty": "Le whisky est obligatoire."
+  }),
 
   rating: Joi.number()
     .min(1)
@@ -15,7 +13,7 @@ export const tastingSchema = Joi.object({
       "number.base": "La note doit être un nombre.",
       "number.min": "La note minimale est 1.",
       "number.max": "La note maximale est 5.",
-      "any.required": "La note est obligatoire.",
+      "any.required": "La note est obligatoire."
     }),
 
   comment: Joi.string()
@@ -23,6 +21,13 @@ export const tastingSchema = Joi.object({
     .allow("")
     .optional()
     .messages({
-      "string.max": "Le commentaire ne peut pas dépasser 500 caractères.",
-    }),
+      "string.max": "Le commentaire ne peut pas dépasser 500 caractères."
+    })
 });
+
+export const tastingUpdateSchema = Joi.object({
+  rating: Joi.number().min(1).max(5).optional(),
+
+  comment: Joi.string().max(500).allow("").optional()
+});
+
