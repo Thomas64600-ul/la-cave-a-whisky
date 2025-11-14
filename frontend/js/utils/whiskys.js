dcc2dc5b8c77c282a92ffc2c68f965e09f9cf191
Thomas64600-ul <thomasdetraversaygmail.comm>
2025-11-14 09:48:02 +0100

    Mise en place du fichier whiskys.js

---
+++ frontend/js/utils/whiskys.js
@@ -0,0 +1,158 @@
+export const whiskys = [
+  {
+    id: 1,
+    name: "High Coast (HAV)",
+    brand: "High Coast",
+    country: "Suède",
+    category: "Single Malt",
+    degree: 48.0,
+    year: 2021,
+    description:
+      "Un single malt suédois élégant de la distillerie High Coast, édition HAV. Notes fruitées (pomme, poire), vanille et miel, avec une touche boisée. Vieilli en fûts de chêne, ce whisky profite du climat nordique pour un vieillissement unique.",
+    image:
+      "https://privatewhiskysociety.com/wp-content/uploads/2021/03/highcoast-hav.jpg"
+  },
+  {
+    id: 2,
+    name: "Aberfeldy 16 ans",
+    brand: "Aberfeldy",
+    country: "Écosse",
+    category: "Single Malt",
+    degree: 40.0,
+    year: null,
+    description:
+      "Aberfeldy 16 ans est un whisky single malt de l’est des Highlands, en Écosse. Sa distillerie est bordée par une source autour de laquelle le sol est riche en minéraux, et plus particulièrement en or. Il se distingue par un caractère franc et sec, mais facile d’accès à la fois. Ce profil unique est notamment la base du fameux blend Dewar’s. Il est vieilli dans des fûts de bourbon reconditionnés, avant de bénéficier d’une finition en fûts de sherry oloroso de premier remplissage.",
+    image:
+      "https://whiskylodge.com/2-medium_default/aberfeldy-16-ans.jpg"
+  },
+  {
+    id: 3,
+    name: "Tchankat",
+    brand: "Tchankat",
+    country: "France",
+    category: "Single Malt",
+    degree: 43.0,
+    year: null,
+    description:
+      "Le Whisky Tchankat Classique est un single malt whisky produit dans le Sud-Ouest de la France, plus précisément dans le Gers, par la distillerie Samalens. Il est élaboré à partir d’un assemblage de 80% de maïs, 10% de seigle et 10% d’orge, cultivés sur les terres gasconnes. Il est ensuite vieilli dans des fûts de vin du Château Gruaud Larose, un grand cru bordelais. Ce whisky révèle des notes fruitées, épicées et toastées, avec une fraîcheur et une intensité remarquables. Il rend hommage aux bergers landais, surnommés “tchankat”, qui gardaient les troupeaux sur des échasses.Whisky Tchankat Classique est un produit de la marque Whisky Tchankat ",
+    image:
+      "https://www.vino-velo.fr/wp-content/uploads/Tchankat-Classique.jpg"
+  },
+  {
+    id: 4,
+    name: "Cardhu 12 ans",
+    brand: "Cardhu",
+    country: "Écosse",
+    category: "Single Malt",
+    degree: 40.0,
+    year: null,
+    description:
+      "Avec une douceur séduisante et soyeuse, voici l'essence même de Cardhu. Cardhu est un single malt produit dans l'une des plus anciennes distilleries écossaises. Son goût rond et fruité est à l'origine du style de la région du Speyside, berceau du whisky. Cardhu 12 ans est l'expression-même du caractère moelleux et parfumé de la distillerie. Vieilli en fût de bourbon, il révèle des notes moelleuses de céréales et de miel. Doux et parfaitement équilibré, ce whisky de 12 ans d'âge ravit les palais les plus experts comme les plus amateurs. Facile à appréhender, il constitue un excellent point de départ pour découvrir l'univers des whiskies premium et libère tout son potentiel servi sec ou sur glace.",
+    image:
+      "https://maisonplouvier.com/930-large_default/cardhu-12-ans.jpg"
+  },
+  {
+    id: 5,
+    name: "Bushmills Original",
+    brand: "Bushmills",
+    country: "Irlande",
+    category: "Single Malt",
+    degree: 40.0,
+    year: null,
+    description:
+      "Bushmills Original est le blend Irish whiskey de référence de la distillerie The Old Bushmills qui s’appuie sur l’héritage d’une fabrication traditionnelle vieille de plus de 400 ans basée sur la triple distillation. Bushmills Original est un mélange unique composé à plus de 50% de whiskey de malt de la distillerie Old Bushmills et de whiskey de grain irlandais - totalement exempt de tourbe.Le whisky de grain a vieilli jusqu’à cinq ans en fûts de chêne spécialement sélectionnés pour donner au whiskey une belle teinte dorée à la lumière. A maturité, ce whiskey de grain est mélangé avec un Single Malt whiskey distillé trois fois afin d’apporter de l’équilibre et de la douceur au Bushmills Original.Ce blend est ainsi complexe avec ses notes riches de fruits, miel et vanille, mais reste très abordable. Un grand classique de l’Irlande qui se révèle idéal à l’apéritif sec, sur glace ou mixé avec de la Ginger Ale. ",
+    image:
+      "https://www.comptoir-irlandais.com/27786/bushmills-original.jpg"
+  },
+  {
+    id: 6,
+    name: "Knockando 12 ans",
+    brand: "Knockando",
+    country: "Écosse",
+    category: "Single Malt",
+    degree: 43.0,
+    year: null,
+    description:
+      "Le Knockando 12 ans d'âge est un single malt léger et fruité. Ce whisky écossais possède une somptueuse robe or pâle qui présage la finesse de ce beau whisky du Speyside. La fraîcheur de ses arômes de fruits accompagnés de note d’amande annonce une bouche délicate. Les notes d’agrumes laissent rapidement la place à l’orge maltée, très présente à la dégustation. Il se prolonge par l’amande et la noisette grillée avant de se terminer sur une notre crémeuse de caramel.",
+    image:
+      "https://media.cdnws.com/_i/21035/400/1/4/knockando-12.jpg"
+  },
+  {
+    id: 7,
+    name: "Talisker Skye",
+    brand: "Talisker",
+    country: "Écosse",
+    category: "Single Malt",
+    degree: 45.0,
+    year: null,
+    description:
+      "Talisker, le rocher escarpé en langue nordique, est la seule distillerie de l'île de Skye, la plus septentrionale des Hébrides intérieures à peine protégée des tempêtes de l'océan atlantique.Etablie en 1830 sur les rives du Loch Harport, elle possède un statut à part dans l'univers du whisky. Son eau douce et tourbée ne provient pas de ruisseaux ou de rivières mais des 21sources souterraines du Hawk Hill à quelques pas de la distillerie. Son single malt, élaboré de façon traditionnelle, possède un caractére huileux reconnaissable dû à son lent refroidissement en sortie d'alambics.Le Talisker Skye est un whisky tourbé, épicé et fumé, avec des notes de fruits mûrs et une finale longue et chaleureuse.",
+    image:
+      "https://d2j6dbq0eux0bg.cloudfront.net/images/28753/talisker-skye.jpg"
+  },
+  {
+    id: 8,
+    name: "West Cork Single Malt",
+    brand: "West Cork",
+    country: "Irlande",
+    category: "Single Malt",
+    degree: 43.0,
+    year: null,
+    description:
+      "Ce blend, nommé Black Cask, est composée à 100% de céréales d’Irlande dont 67% de grain et 33% d’orge maltée. Il se distingue par la maîtrise de son vieillissement dans différents types de fûts : après un vieillissement de 3 ans dans des ex-fûts de bourbon du Kentucky, il est affiné pendant 6 mois dans des fûts de premier remplissage brûlés deux fois. Cette technique permet d’atteindre un niveau très élevé de bousinage de 5 sur une échelle de 7. Ce taux de chauffe est encore plus élevé que celui pour le célèbre Ardbeg Alligator permettant ainsi au Black Cask de vous faire découvrir un vieillissement unique.",
+    image:
+      "https://www.jardinouvrillon.fr/5285-large_default/west-cork-single-malt.jpg"
+  },
+  {
+    id: 9,
+    name: "Rock Island",
+    brand: "Douglas Laing",
+    country: "Écosse",
+    category: "Blended Malt",
+    degree: 46.8,
+    year: null,
+    description:
+      "Rock Island, anciennement connu sous le nom de Rock Oyster, est un whisky écossais issu de la région des îles, reconnu pour son caractère distinctif et sa complexité. Elaboré par le célèbre assembleur Douglas Laing, ce Blended Malt combine des whiskies provenant de plusieurs distilleries des îles écossaises, ce qui lui confère une richesse aromatique exceptionnelle. Il présente des arômes puissants de fruits mûrs, accompagnés de notes maritimes salines, fumées et légèrement tourbées, typiques des whiskies d'île. En bouche, Rock Island offre une expérience gustative équilibrée, mêlant douceur, épices et une finale longue et chaleureuse. C'est un choix idéal pour les amateurs de whiskies complexes et bien structurés, à déguster pur ou avec un peu d'eau pour révéler toute sa palette aromatique.",
+    image:
+      "https://www.prestigewhisky.fr/6833-large_default/rock-island.jpg"
+  },
+  {
+    id: 10,
+    name: "Lascaw 15 ans",
+    brand: "Lascaw",
+    country: "France",
+    category: "Single Malt",
+    degree: 40.0,
+    year: null,
+    description:
+      "Toutes les saveurs et la puissance du Whisky Lascaw 15 ans d’âge reposent sur sa composition à base exclusive de whisky de malt écossais. Son caractère exceptionnel provient de son vieillissement pendant 15 ans et de son affinage ultime par un passage en fûts de chêne ayant contenu notre spécialité du Périgord au parfum de truffe. ",
+    image:
+      "https://media.carrefour.fr/medias/625d8bd96aac3f67/lascaw-15.jpg"
+  },
+  {
+    id: 11,
+    name: "Aberlour 12 ans",
+    brand: "Aberlour",
+    country: "Écosse",
+    category: "Single Malt",
+    degree: 40.0,
+    year: null,
+    description:
+      "Aberlour 12 Ans Un-chillfiltered est un single malt du Speyside non filtré à froid. La non-filtration à froid préserve intacte l’intensité des saveurs, révélant ainsi un Single Malt (titrant à 48°) généreux et intense. Vieilli 12 ans en fûts de sherry et en fûts de bourbon, c'est un whisky riche aux arômes fruités de prune, équilibrés par des saveurs riches de chocolat, toffee, cannelle et raisins secs.",
+    image:
+      "https://whiskyparis.com/cdn/shop/files/wh0976-aber.jpg"
+  },
+  {
+    id: 12,
+    name: "Togouchi Single Malt",
+    brand: "Togouchi",
+    country: "Japon",
+    category: "Single Malt",
+    degree: 40.0,
+    year: null,
+    description:
+      "Kiwami (signifiant «suprême») est un whisky japonais de la maison Chugoku Jozo. Les whiskies Togouchi ont pour particularité d'être uniquement vieillis, assemblés et dilués au Japon. La distillerie mûrit ses whiskies dans un tunnel de 361 mètres de long offrant des conditions idéales de maturation. Au nez, il révèle des notes de noix et d'herbes fraîches, en bouche, il est gourmand: chocolat et noisette agrémentés de miel et d'une note de poivre noir.",
+    image:
+      "https://www.maisonlauze.com/1366-pdt_771/togouchi-single-malt.jpg"
+  }
+];
